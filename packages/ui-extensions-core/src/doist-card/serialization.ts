2f59c832191c2bdbda9ab40d15ecf4bcd29523e8
Scott Lovegrove <scott@ferretlabs.com>
2022-11-28 11:47:48 +0000

    chore: Adds JSDoc for SerializableObject

--- packages/ui-extensions-core/src/doist-card/serialization.ts
+++ packages/ui-extensions-core/src/doist-card/serialization.ts
@@ -7,6 +7,12 @@ export class SerializableObject {
         return serialize(this, true) as string
     }
 
+    /**
+     * Easily create an instance of an object while passing in properties of the object.
+     * @param this the type from which the object is created
+     * @param props the props of the object to be created
+     * @returns an instance of the type from which the object is created
+     */
     static from<T extends SerializableObject>(this: new () => T, props: Props<T>): T {
         const o = new this()
         Object.assign(o, props)
