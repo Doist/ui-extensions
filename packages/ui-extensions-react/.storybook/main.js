93e423da136a1491c20b624586f2c8fe89aa69b1
Pedro Alves <pedroalves0@gmail.com>
2023-09-26 09:51:23 +0200

    chore: update Reactist to v21 and bump version to 10.0.0 (#55)
    * chore: update Reactist to v21
    
    * build: bump version to 10.0.0

--- packages/ui-extensions-react/.storybook/main.js
+++ packages/ui-extensions-react/.storybook/main.js
@@ -2,6 +2,9 @@ module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
     webpackFinal: async (config) => {
+        // Skip transpilation for node_modules, except for non-transpiled dependencies
+        config.module.rules[0].exclude = /node_modules\/(?!.*ariakit.*).*/
+
         config.module.rules.push({
             test: /\.(ts|tsx)$/,
             use: {
