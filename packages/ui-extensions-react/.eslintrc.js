9bad7ea02e41da7c88fca7c89eeae6f4032f8fd5
Frankie Yan <frankie@doist.com>
2025-11-04 20:37:44 -0800

    Set no-restricted-imports to warning The rule enforces that React isn't imported as a namespace as it's no longer needed

--- packages/ui-extensions-react/.eslintrc.js
+++ packages/ui-extensions-react/.eslintrc.js
@@ -10,5 +10,6 @@ module.exports = {
         // ref: https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
         'react/jsx-uses-react': 'off',
         'react/react-in-jsx-scope': 'off',
+        'no-restricted-imports': 'warn',
     },
 }
